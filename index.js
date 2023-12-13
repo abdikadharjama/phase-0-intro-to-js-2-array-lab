3133e5320f327fdc32f3d3783580a8953cc1c540
Abdikadhar <Abdikadharjama@gmail.com>
2023-12-13 12:52:30 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Write your solution here!
+
+// is assigned an initial value of ["Milo", "Otis", "Garfield"]
+const cats = ["Milo", "Otis", "Garfield"];
+// appends a cat to the end of the cats array
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+
+function appendCat(name) {
+  return [...cats, name];
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+
+function prependCat(name) {
+  return [name, ...cats];
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+function removeLastCat() {
+  return cats.slice(0, -1);
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+function removeFirstCat() {
+  return cats.slice(1);
+}
+
+module.exports = {
+  cats,
+  destructivelyAppendCat,
+  appendCat,
+  destructivelyPrependCat,
+  prependCat,
+  destructivelyRemoveLastCat,
+  removeLastCat,
+  destructivelyRemoveFirstCat,
+  removeFirstCat,
+};
